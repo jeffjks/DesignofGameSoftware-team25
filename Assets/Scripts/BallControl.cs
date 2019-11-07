a1689e810daeff4ca4c034ffe864a266c1fc8bf3
jeff_jks <jeff_jks@naver.com>
2019-11-07 12:03:25 +0900

    First Commit

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public BallState m_BallState;
+    public Rigidbody m_RigidBody;
+    public Transform m_BallCamera;
+    public float m_CameraRotationSpeed;
+    
+    private float m_CameraRotation;
+    private int m_ShotPower;
+
+    void Update()
+    {
+        float xRot = Input.GetAxisRaw("Mouse X");
+
+        PreformCameraRotation(xRot);
+
+        if (m_BallState.m_Controlable) {
+            if (Input.GetButtonDown("Fire1")) {
+                m_ShotPower = 0;
+            }
+            else if (Input.GetButton("Fire1")) {
+                m_ShotPower++;
+            }
+            else if (Input.GetButtonUp("Fire1")) {
+                PerformShot();
+            }
+        }
+    }
+    
+    void PreformCameraRotation(float xRot)
+    {
+        m_CameraRotation += xRot * m_CameraRotationSpeed;
+        m_BallCamera.eulerAngles = new Vector3(0f, m_CameraRotation, 0f);
+        //m_BallCamera.Rotate(Vector3.up * m_CameraRotationSpeed * xRot, Space.World);
+    }
+
+    void PerformShot()
+    {
+        m_BallState.m_Controlable = false;
+        Vector3 force = m_BallCamera.forward * m_ShotPower * 0.01f;
+        Debug.Log(force);
+        m_RigidBody.AddForce(force, ForceMode.Impulse);
+    }
+}
+
---
+++ Assets/Scripts/BallState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallState : MonoBehaviour
+{
+    public Rigidbody m_RigidBody;
+    public float m_AirDrag, m_GroundDrag;
+    
+    private RaycastHit m_RaycastHit;
+    private float m_MaxDistance;
+    private bool m_IsGround;
+
+    [HideInInspector] public bool m_Controlable = true;
+
+    void Start()
+    {
+        m_MaxDistance = transform.localScale.y * 0.5f + 0.1f;
+    }
+
+    void Update()
+    {
+        Debug.Log(m_IsGround);
+
+        if (m_IsGround) {
+            m_RigidBody.drag = m_GroundDrag;
+        }
+        else {
+            m_RigidBody.drag = m_AirDrag;
+        }
+
+        if (m_RigidBody.velocity == Vector3.zero) {
+            m_Controlable = true;
+        }
+    }
+
+    // Check Collision with Ground
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain") {
+            m_IsGround = true;
+        }
+    }
+     
+     void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain") {
+            m_IsGround = false;
+        }
+    }
+}
+
